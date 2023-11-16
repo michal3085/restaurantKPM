d620b9af93d985ed2d834b19b7adaf90ff7a262f
michal3085 <michal3085@gmail.com>
2023-11-16 20:58:44 +0100

    RES-4

--- resources/views/main/sections/_drinks.blade.php
+++ resources/views/main/sections/_drinks.blade.php
@@ -11,6 +11,8 @@
                     <li data-filter=".filter-drink">{{ __('Drinks') }}</li>
                     <li data-filter=".filter-vodka">{{ __('Vodka') }}</li>
                     <li data-filter=".filter-beer">{{ __('Beer') }}</li>
+                    <li data-filter=".filter-whisky">{{ __('Whisky') }}</li>
+                    <li data-filter=".filter-brandy">{{ __('Cognac, Brandy') }}</li>
                 </ul>
             </div>
         </div>
@@ -19,47 +21,172 @@
 
 
             <div class="col-lg-6 drink-item filter-drink">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
                     <a href="#">Drink</a><span>12 PLN</span>
                 </div>
             </div>
 
+{{--     BEERS       --}}
             <div class="col-lg-6 drink-item filter-beer">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
                     <a href="#">Książęce Lager 0,5 l</a><span>12 PLN</span>
                 </div>
             </div>
 
             <div class="col-lg-6 drink-item filter-beer">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
                     <a href="#">Książęce Lager 0,3 l</a><span>10 PLN</span>
                 </div>
             </div>
 
             <div class="col-lg-6 drink-item filter-beer">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
-                    <a href="#">Kozel lane / draft 0,5 l</a><span>13 PLN</span>
+                    <a href="#">Kozel {{ __('draft') }} 0,5 l</a><span>13 PLN</span>
                 </div>
             </div>
 
             <div class="col-lg-6 drink-item filter-beer">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
-                    <a href="#">Kozel lane / draft 0,3 l</a><span>11 PLN</span>
+                    <a href="#">Kozel {{ __('Red') }} 0,3 l</a><span>11 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Żywiec butelka 0,5 l</a><span>11 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Lech butelka 0,5 l</a><span>9 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Sowie 0,5 l</a><span>16 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Książęce {{ __('Red') }} Lager - butelka 0,5 l</a><span>12 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Książęce {{ __('Dark') }} - butelka 0,5 l</a><span>12 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Książęce {{ __('Wheat') }} butelka 0,5 l</a><span>12 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-beer">
+                <div class="drink-content">
+                    <a href="#">Miedzianka</a><span>17 PLN</span>
+                </div>
+            </div>
+
+{{--    END BEERS        --}}
+
+{{--    VODKA        --}}
+
+            <div class="col-lg-6 drink-item filter-vodka">
+                <div class="drink-content">
+                    <a href="#">Stock prestige 40 ml</a><span>9 PLN</span>
                 </div>
             </div>
 
             <div class="col-lg-6 drink-item filter-vodka">
-                <img src="assets/img/menu/REVEMENU1S.jpg" class="drink-img" alt="">
                 <div class="drink-content">
-                    <a href="#">Wóda</a><span>12 PLN</span>
+                    <a href="#">Stock prestige De lux 0,5 l</a><span>80 PLN</span>
                 </div>
             </div>
 
+            <div class="col-lg-6 drink-item filter-vodka">
+                <div class="drink-content">
+                    <a href="#">Wyborowa 40 ml</a><span>9 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-vodka">
+                <div class="drink-content">
+                    <a href="#">Wyborowa 0,5 l</a><span>80 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-vodka">
+                <div class="drink-content">
+                    <a href="#">Finlandia 40 ml</a><span>10 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-vodka">
+                <div class="drink-content">
+                    <a href="#">Finlandia 0,5 l</a><span>100 PLN</span>
+                </div>
+            </div>
+
+{{--END VODKA--}}
+{{--WHISKY--}}
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Grant's 40 ml</a><span>12 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Johny Walker Red 40 ml</a><span>14 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Johny Walker Black 40 ml</a><span>16 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Ballantines 40 ml</a><span>14 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Jack Daniels 40 ml</a><span>14 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Chivas Regal 12 40 ml</a><span>16 PLN</span>
+                </div>
+            </div>
+
+            <div class="col-lg-6 drink-item filter-whisky">
+                <div class="drink-content">
+                    <a href="#">Jemeson 40 ml</a><span>16 PLN</span>
+                </div>
+            </div>
+
+{{--END WHISKY--}}
+{{--BRANDY--}}
+
+            <div class="col-lg-6 drink-item filter-brandy">
+                <div class="drink-content">
+                    <a href="#">Kazbek Peak 40 ml</a><span>20 PLN</span>
+                </div>
+            </div>
+
+{{--END BRANDY--}}
         </div>
 
     </div>
